b4402b07f4120ad6414ade3347183bf1740923d8
bruteForceBilly <bruteForceBilly@protonmail.com>
2021-06-04 11:46:11 +0200

    large pixel grid

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,8 @@ export default {
   },
   data() {
     return {
-      width: 320,
-      height: 320,
+      width: 1440,
+      height: 1440,
       backgroundColor: 0xFFFFFF,
       resolution: 1,
     };
@@ -35,10 +35,30 @@ export default {
   methods: {
     draw(g) {
       g.clear();
-      g.lineStyle(1, 0x79848D);
-      g.beginFill(0xF0F3F9);
-      g.drawRect(1, 0, 8, 8);
-    }
+      // g.lineStyle(1, 0x79848D);
+      // g.beginFill(0xF0F3F9);
+      // g.drawRect(1, 0, 8, 8);
+    
+      let rows = 144, cols = 144;
+      let width = 8, height = 8;
+      
+      const total = rows * cols;
+      
+      let i = 0;
+
+      while(i<total){
+        // ((Math.floor(i/256)) * 8) + 32
+        // (Math.floor(i/rows) * height) + margin
+        let posX = Math.floor(i/rows) * height;
+        let posY = (i % rows) * width;                      
+          g.lineStyle(1, 0x79848D);
+          g.beginFill(0xF0F3F9);
+          // x y w h
+          g.drawRect(posX, posY, width, height);
+  
+          i++;
+      }
+    }    
   },
 };
 </script>
