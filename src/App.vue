b476f2053b5f12c4243c16c6680216fa6e0dbc42
bruteForceBilly <bruteForceBilly@protonmail.com>
2021-06-04 15:46:08 +0200

    pixi wip

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <p-application
+     <renderer>
+      <container
+        :x="200" :y="400" @pointerdown="scaleObject"
+      >
+        <sprite :x="0" :y="0" imagePath="./assets/vue-logo.png"/>
+      </container>
+    </renderer>
+    <!-- <p-application
       :width="width"
       :height="height"
       :backgroundColor="backgroundColor"
@@ -8,23 +15,33 @@
       :skipHello="true"
     >
       <p-container>
-        <p-graphics :draw="draw"/>
+        <p-graphics 
+        :draw="draw" 
+        v-for="n in 1" 
+        :key="n"
+        />
       </p-container>
-    </p-application>
+    </p-application> -->
   </div>
 </template>
 
 <script>
-import { PApplication, PContainer, PGraphics } from "vue-pixi-wrapper";
-import * as PixiJSGrid from 'pixijs-grid'
-import * as PIXI from 'pixi.js';
+import Renderer from './components/PIXIRenderer.vue'
+import Sprite from './components/PIXISprite.vue'
+import Container from './components/PIXIContainer.vue'
+
+//import { PApplication, PContainer, PGraphics } from "vue-pixi-wrapper";
+//import * as PIXI from 'pixi.js';
 
 export default {
   name: "App",
   components: {
-    PApplication,
-    PContainer,
-    PGraphics
+    Renderer,
+    Sprite,
+    Container
+    // PApplication,
+    // PContainer,
+    // PGraphics
   },
   data() {
     return {
@@ -32,45 +49,36 @@ export default {
       height: 1440,
       backgroundColor: 0xFFFFFF,
       resolution: 1,
-      posX: 0,
-      posY: 0,
-      rows: 12,
-      cols: 12,
-      seatWidth: 32,
-      seathHeight: 32
     };
   },
   methods: {
-    draw(g) {
-      g.clear();    
-      g.lineStyle(1, 0x79848D);
-      g.beginFill(0xF0F3F9);
-      // x y w h
-      g.hitArea = new PIXI.Rectangle(this.posX, this.posY, this.width, this.height);
-      g.drawRect(this.posX, this.posY, this.width, this.height);
 
-      g.interactive = true;
-      g.mouseover = () => {
-        console.log("mouse over")
-      }
+    scaleObject(container) {
+      container.scale.x *= 1.25;
+      container.scale.y *= 1.25;
+    },
 
-      g.mouseout = () => {
-        console.log("mouse leave")
-      }
-
-    }    
+    // draw(g) {
+ 
+    //   // g.clear();    
+    //   // g.lineStyle(1, 0x79848D);
+    //   // g.beginFill(0xF0F3F9);
+    //   // // x y w h
+    //   // g.drawRect(g.x, g.y, g.width, g.height);
+    //   // g.endFill();
+    // }    
   },
   mounted () {
-      const total = this.rows * this.cols;
-      let i = 0;
-        while(i<total){
-        // ((Math.floor(i/256)) * 8) + 32
-        // (Math.floor(i/rows) * height) + margin
-        this.posX = Math.floor(i/this.rows) * this.height;
-        this.posY = (i % this.rows) * this.width;                              
-        this.draw()
-        i++;
-      }
+      // const total = this.rows * this.cols;
+      // let i = 0;
+      //   while(i<total){
+      //   // ((Math.floor(i/256)) * 8) + 32
+      //   // (Math.floor(i/rows) * height) + margin
+      //   this.posX = Math.floor(i/this.rows) * this.height;
+      //   this.posY = (i % this.rows) * this.width;                              
+      //   this.draw()
+      //   i++;
+      // }
       
   },
 };
---
+++ src/components/SeatMap.vue
@@ -0,0 +1,20 @@
+<script>
+import * as PIXI from 'pixi.js';
+
+let app = new PIXI.Application({width: 256, height: 256});
+
+console.log("app", app.view)
+
+
+export default {
+  name: 'seatMap',
+  render: function (createElement) {
+        return createElement(app.view)
+    }
+};
+</script>
+
+
+
+
+
